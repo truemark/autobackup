4e83b6d1588cfd1cc5860dce16e21e633cc13b5f
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-05-10 20:04:08 -0600

    Fixing construct location and adding metadata

--- lib/autobackup-stack.ts
+++ lib/autobackup-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import {AutoBackup} from "../test/autobackup-construct"
+import {AutoBackup} from "./autobackup-construct"
+import * as p from "../package.json";
 
 export class AutoBackupStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    this.addMetadata("Version", p.version);
+    this.addMetadata("Name", p.name);
+    this.addMetadata("RepositoryType", p.repository.type);
+    this.addMetadata("Repository", p.repository.url);
+    this.addMetadata("Homepage", p.homepage);
     new AutoBackup(this, "AutoBackup", {});
-
   }
 }
